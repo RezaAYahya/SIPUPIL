c2698a5611f4a2adb8856c31eb1aea3f5180de39
RezaAYahya <rezaahmadyahya@gmail.com>
2021-03-30 15:51:43 +0700

    add controller mahasiswa and edit User

---
+++ Codingan/application/controllers/Mahasiswa.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Mahasiswa extends CI_Controller
+{
+    public function index()
+    {
+        echo 'ini method index';
+    }
+
+    public function Update()
+    {
+        echo 'ini method update';
+    }
+}
--- Codingan/application/controllers/User.php
+++ Codingan/application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller
+class User extends CI_Controller
 {
     public function index()
     {
