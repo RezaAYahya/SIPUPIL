c8f8e088c1f8fd5b4ce954d21609250582acfeb0
izarwh <ijul.harahap@gmail.com>
2021-04-20 16:12:17 +0700

    edit mahasiswa

--- Codingan/application/controllers/Mahasiswa.php
+++ Codingan/application/controllers/Mahasiswa.php
@@ -10,6 +10,43 @@ class Mahasiswa extends CI_Controller
 
     public function Update()
     {
-        echo 'ini method update';
+
+      $user = $this->session->userdata('username');
+        if ($user != NULL) {
+            $d_mhs['editMhs'] = $this->mahasiswa_model->getprofile('cicilalang@gmail.com');
+
+            $this->form_validation->set_rules('name', 'Name', 'required');
+            $this->form_validation->set_rules('password', 'Password', 'required|trim');
+            $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+            $this->form_validation->set_rules('number', 'Number', 'required');
+            $this->form_validation->set_rules('alamat', 'alamat', 'required');
+            $this->form_validation->set_rules('asalsma', 'asalsma', 'required');
+            $this->form_validation->set_rules('ttl', 'ttl', 'required');
+
+            if ($this->form_validation->run() == false) {
+                $this->load->view('templates/auth_header');
+                $this->load->view('mahasiswa/edit_mahasiswa',$d_mhs);
+                $this->load->view('templates/auth_footer');
+            } else {
+                $data = [
+                    'username' => $user,
+                    'password' => $this->input->post('password'),
+                    'name' => $this->input->post('name'),
+                    'email' => $this->input->post('email'),
+                    'nomortelefon' => $this->input->post('nomortelefon')
+                    'asalsma' => $this->input->post('asalsma')
+                    'ttl' => $this->input->post('ttl')
+                    'alamat' => $this->input->post('alamat')
+                ];
+
+                $this->mahasiswa_model->setprofile($data);
+
+
+                redirect('/mahasiswa/edit_mahasiswa/');
+            }
+        } else {
+            redirect('/Welcome');
+        }
     }
+
 }
--- Codingan/application/views/mahasiswa/edit_mahasiswa.php
+++ Codingan/application/views/mahasiswa/edit_mahasiswa.php
@@ -1,3 +1,4 @@
+<?php echo $editMhs['Name']; ?>
     <div class="kotak-edit">
         <h1>Edit Profile Mahasiswa</h1>
 
@@ -16,35 +17,35 @@
                     <div class="form-group-row">
                         <label for="name" class="col-sm col-form-label">Nama Mahasiswa</label>
                         <div class="col-sm">
-                            <input type="text" class="form-control" id="name" name="name" disabled>
+                            <input type="text" class="form-control" id="name" name="name" value="<?=$editMhs['name']?>">
                         </div>
                     </div>
 
                     <div class="form-group-row">
                         <label for="email" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm">
-                            <input type="text" class="form-control" id="email" name="email">
+                            <input type="text" class="form-control" id="email" name="email" value="<?=$editMhs['email']?>" />
                         </div>
                     </div>
 
                     <div class="form-group-row">
                         <label for="ttl" class="col-form-label">Tempat Tanggal Lahir</label>
                         <div class="col-sm">
-                            <input type="text" class="form-control" id="ttl" name="ttl">
+                            <input type="text" class="form-control" id="ttl" name="ttl" value="<?=$editMhs['ttl']?>" />
                         </div>
                     </div>
 
                     <div class="form-group-row">
                         <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
                         <div class="col-sm">
-                            <input type="text" class="form-control" id="alamat" name="alamat">
+                            <input type="text" class="form-control" id="alamat" name="alamat" value="<?=$editMhs['ttl']?>" />
                         </div>
                     </div>
 
                     <div class="form-group-row">
                         <label for="notelp" class="col-form-label">Nomor Telpon</label>
                         <div class="col-sm">
-                            <input type="text" class="form-control" id="notelp" name="notelp">
+                            <input type="text" class="form-control" id="nomortelefon" name="nomortelefon" value="<?=$editMhs['nomortelefon']?>" />
                         </div>
                     </div>
 
