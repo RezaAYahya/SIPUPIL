e0f2ab830b09a1172cda73df66f011cdeb46c56f
izarwh <ijul.harahap@gmail.com>
2021-04-20 14:34:48 +0700

    Misah view

---
+++ Codingan/application/views/auth/loginmhs.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <img src="<?= base_url('assets'); ?>/img/UPIL.png" class="col-lg-6 img-thumbnail">
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h2 text-gray-900 mb-4">Login Mahasiswa</h1>
+                                </div>
+
+                                <?= $this->session->flashdata('message'); ?>
+                                <form class="user" method="POST" action="<?= base_url('login/mahasiswalog'); ?>">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." name="email" value="<?= set_value('email'); ?>">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>') ?>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>') ?>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                    <hr>
+                                </form>
+                                <div class="text-center">
+                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
---
+++ Codingan/application/views/auth/menu_login.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <img src="<?= base_url('assets'); ?>/img/UPIL.png" class="col-lg-6 img-thumbnail">
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h2 text-gray-900 mb-4">Menu Login</h1>
+                                </div>
+                                <div class="col-md-12">
+                                    <button class="btn btn-primary btn-user btn-block" onclick="location.href='<?= base_url();?>login/mahasiswalog'" class="small" >Login Mahasiswa</button>
+                                    <button class="btn btn-primary btn-user btn-block" onclick="location.href='<?= base_url();?>login/dosenlog'" class="small" >Login Dosen</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
