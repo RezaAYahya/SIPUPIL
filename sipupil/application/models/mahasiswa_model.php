9342d27815af869af1a6c32f5d2b1ef4e5199763
izarwh <ijul.harahap@gmail.com>
2021-05-04 15:23:12 +0700

    komen di model

--- sipupil/application/models/mahasiswa_model.php
+++ sipupil/application/models/mahasiswa_model.php
@@ -5,8 +5,9 @@ class mahasiswa_model extends CI_Model
 {
     public function verifyMhs($data)
     {
-        //dari tb_member karena sudah ada pemilihan menu dari awal
+        // memverifikasi email dan password, apabila data ada di database, maka akan mengembalikan seluruh tabel data mahasiswa yang sesuai
         $query = $this->db->where("email", $data['email'])->where("password", $data['password'])->get("mahasiswa");
+        // conditional, apabila data ditemukan maka num_rows() > 0. apabila tidak ada maka num_rows == 0
         if ($query->num_rows() > 0) {
             return true;
         } else {
@@ -16,7 +17,10 @@ class mahasiswa_model extends CI_Model
 
     public function getprofile($user)
     {
-        $email = $this->db->where('email', $user['email'])->get('mahasiswa')->row_array(); #untuk searching menggunakan nim
+        // pada class diagram, kami menentukan NIM sebagai parameter pencarian profile,namu pada menu login kami memasukkan email sebagai ketentuan untuk LogicException
+        //maka dari email kita mencari NIM, setelah itu dari NIM kita dapat mencari data.
+        $email = $this->db->where('email', $user['email'])->get('mahasiswa')->row_array();
+        // mencari data dari nim yang didapatkan pada query di atas
         $query = $this->db->where('NIM', $email['NIM'])->get('mahasiswa')->row_array();
         if($query){
           return $query;
--- sipupil/application/models/registrasimk_model.php
+++ sipupil/application/models/registrasimk_model.php
@@ -3,14 +3,29 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class mahasiswa_model extends CI_Model
 {
+    //Fungsi yang mengembalikan data yang row array
     public function getpilihan($data)
     {
         //dari tb_member karena sudah ada pemilihan menu dari awal
         $NIM = $this->db->where("NIM", $data['NIM'])->get("pilihan");
         if ($query->num_rows() > 0) {
+            // mengembalikan data pilihan matkul yang tersedia
             return $query->row_array();
         } else {
             return false;
         }
+    }
+    // memasukkan data matkul yang menjadi pilihan mahasiswa
+    public function ambil($data){
+      $a = $this->db->where('Mata_Kuliah', $data['matakuliah'])->get('matakuliah')->row_array();
+      $b = $this->db->where('ID_registrasi', $a['ID_Matkul'])->get('pilihan')->row_array();
+      $c = $this->db->query('select max(ID_terpilih) from terpilih')
+
+      $this->db->query("INSERT INTO `terpilih` (`ID_terpilih`, `ID_Registrasi`, `NIM`) VALUES ($VarA,$VarB, '1301180063');");
+    }
+
+    //menghapus pilihan sesuai dengan nama mata kuliah yang dipilih.
+    public function drop(){
+      $this->db->query("DELETE terpilih from terpilih INNER JOIN pilihan ON terpilih.ID_Registrasi=pilihan.ID_Registrasi INNER JOIN matakuliah ON pilihan.ID_Matkul=matakuliah.Mata_Kuliah WHERE matakuliah.Mata_Kuliah like 'kalkulus");
     }
 }
