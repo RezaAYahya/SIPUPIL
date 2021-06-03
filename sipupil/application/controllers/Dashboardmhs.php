42153236914f35bfc7a3170be9f5da5b04e9e17e
Julizar <ijul.harahap@gmail.com>
2021-06-03 16:08:42 +0700

    Update Controller

--- sipupil/application/controllers/Dashboardmhs.php
+++ sipupil/application/controllers/Dashboardmhs.php
@@ -3,8 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboardmhs extends CI_Controller {
 
+	public function __construct()
+  {
+    parent::__construct();
+
+    $this->load->model('mahasiswa_model');
+    $this->load->library('session');
+
+  }
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		if($this->session->userdata('username') != null){
+			$user['email'] = $this->session->userdata('email');
+			$user_d['data_m'] = $this->mahasiswa_model->getprofile($user);
+			if ($user_d['data_m'] != null){
+
+				$this->load->view('mahasiswa/Dashboard',$user_d);
+				$this->load->View('templates/dashboard_footer');
+			}
+
+		}
+
 	}
 }
--- sipupil/application/controllers/Dosen.php
+++ sipupil/application/controllers/Dosen.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Mahasiswa extends CI_Controller
+class Dosen extends CI_Controller
 {
     public function __construct(){
       parent::__construct();
       $this->load->library('session');
-      $this->load->model('mahasiswa_model');
+      $this->load->model('dosen_model');
     }
 
     public function index()
@@ -19,7 +19,7 @@ class Mahasiswa extends CI_Controller
         if ($user_d != null){
           // echo $user_d['username'];
           $this->load->view('templates/dashboard_header',$user_d);
-          $this->load->view('mahasiswa/Dashboard',$user_d);
+          $this->load->view('dosen/Dashboard',$user_d);
           $this->load->View('templates/dashboard_footer');
         } else{
           echo "salah";
@@ -32,14 +32,7 @@ class Mahasiswa extends CI_Controller
 
     public function registrasi(){
 
-      $user['email'] = $this->session->userdata('email');
-      $user_d['data_m'] = $this->mahasiswa_model->getprofile($user);
 
-      if ($user_d!=null){
-        $this->load->view('templates/dashboard_header',$user_d);
-        $this->load->view('mahasiswa/registrasi',$user_d);
-        $this->load->View('templates/dashboard_footer');
-      }
     }
     public function editmahasiswa(){
 
